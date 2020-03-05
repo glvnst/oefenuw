b65fc5de1bb8d55415e0bf2478ef838afbf4d9b3
glvnst <glvnst@users.noreply.github.com>
2020-03-05 10:46:08 +0100

    Slightly better shim

--- v1/src/views/About.vue
+++ v1/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="content">
+      <h1>About the Site</h1>
+      <p>
+        This site is here to help you practice some parts of the Dutch language.
+        It is made by some friends in Belgium who are taking Dutch 1.1.
+      </p>
+
+      <h2>Credits</h2>
+      <p>Coming soon?</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About"
+};
+</script>
