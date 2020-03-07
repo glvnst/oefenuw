90f35971bfc90d2e306509685a6e0e938998d28c
glvnst <glvnst@users.noreply.github.com>
2020-03-07 23:28:46 +0100

    Making this the home page now, applying l10n.

--- v1/src/views/About.vue
+++ v1/src/views/About.vue
@@ -1,20 +1,80 @@
 <template>
-  <div class="about">
-    <div class="content">
-      <h1>About the Site</h1>
-      <p>
-        This site is here to help you practice some parts of the Dutch language.
-        It is made by some friends in Belgium who are taking Dutch 1.1.
-      </p>
+  <div id="aboutMain" class="columns is-multiline is-mobile is-centered">
+    <div class="column is-full">
+      <div class="content">
+        <h1>{{ $t("about_our_games") }}</h1>
+      </div>
+    </div>
 
-      <h2>Credits</h2>
-      <p>Coming soon?</p>
+    <div v-for="(route, i) in gameRoutes" :key="i" class="column">
+      <div class="content">
+        <h2>
+          <router-link :to="route.path" class="link"
+            >&#127918; {{ route.name }}</router-link
+          >
+        </h2>
+        <p>
+          {{ $t("description_" + route.path.substring(1)) }}
+        </p>
+      </div>
+    </div>
+
+    <div class="column is-full">
+      <div class="box has-background-white-ter has-text-grey-darker">
+        <div class="content">
+          <h3>{{ $t("about_title") }}</h3>
+          <p>{{ $t("about_description") }}</p>
+          <p v-html="$t('about_oss', { repo: repoLink($t('about_repo')) })"></p>
+          <!-- <p>
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+          >
+            <img
+              alt="Creative Commons License"
+              style="border-width:0"
+              src="/by-nc-sa-40-88x31.png"
+            />
+          </a>
+          <br />
+          The images used in this work are licensed under a
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+            >Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+            International License</a
+          >.
+        </p> -->
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "About"
+  name: "About",
+  computed: {
+    gameRoutes() {
+      return this.$router.options.routes.filter(route => {
+        return route.path != "/";
+      });
+    }
+  },
+  methods: {
+    repoLink(repoLiteral) {
+      return (
+        '<a href="https://github.com/glvnst/oefenuw/" target="_blank">' +
+        repoLiteral +
+        "</a>"
+      );
+    }
+  }
 };
 </script>
+
+<style>
+#aboutMain {
+  margin: 1.5em 3em 0 3em;
+}
+</style>
