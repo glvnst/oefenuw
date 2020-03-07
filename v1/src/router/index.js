1db6857f61ceab5996ed1e7aba86cc35ca46a3a4
glvnst <glvnst@users.noreply.github.com>
2020-03-07 23:26:10 +0100

    removing the home page, making / point to the About page

--- v1/src/router/index.js
+++ v1/src/router/index.js
@@ -1,35 +1,30 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import Home from "../views/Home.vue";
+// import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "Over",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
-  },
-  {
-    path: "/about",
-    name: "About",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "About" */ "../views/About.vue")
   },
   {
     path: "/alfabet",
     name: "Alfabet",
     component: () =>
-      import(/* webpackChunkName: "alfabet" */ "../views/Alfabet.vue")
+      import(/* webpackChunkName: "Alfabet" */ "../views/Alfabet.vue")
   },
   {
     path: "/ditofdat",
     name: "Dit of Dat",
     component: () =>
-      import(/* webpackChunkName: "ditofdat" */ "../views/DitOfDat.vue")
+      import(/* webpackChunkName: "DitOfDat" */ "../views/DitOfDat.vue")
   }
 ];
 
